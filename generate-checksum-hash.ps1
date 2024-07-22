c838b3a5803188062d92d2012c749b512d6e0c15
Paul Sellick <paul.sellick@gmail.com>
2024-07-22 10:34:25 +1000

    Update and rename get-hash.ps1 to generate-checksum-hash.ps1

---
+++ generate-checksum-hash.ps1
@@ -0,0 +1,3 @@
+﻿$hashfile = "c:\temp\extract\sha56sums.txt"
+$sourcepath = "c:\temp\extract\source"
+Get-ChildItem -Path $sourcepath -Recurse -Exclude $hashfile | Get-FileHash | Out-File $hashfile
--- get-hash.ps1
+++
@@ -1,4 +0,0 @@
-﻿$hashfile = "sha56sums.txt"
-Get-ChildItem -Path . -Recurse -Exclude $hashfile | Get-FileHash | Out-File $hashfile
-
-
